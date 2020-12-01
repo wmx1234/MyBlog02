286528039483188f225dca9c2edc00290a3f78b8
wangmx996 <wangmx_he@163.com>
2020-12-01 22:15:59 +0800

    2020-1

---
+++ src/main/java/com/xiao/blog/config/BlogProperties.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2018-2020 stylefeng & fengshuonan (sn93@qq.com)
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xiao.blog.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * guns项目配置
+ *
+ * @author stylefeng
+ * @Date 2017/5/23 22:31
+ */
+@Component
+@ConfigurationProperties(prefix = com.xiao.blog.config.BlogProperties.PREFIX)
+public class BlogProperties {
+
+    public static final String PREFIX = "blog";
+
+    private Boolean kaptchaOpen = false;
+
+    public Boolean getKaptchaOpen() {
+        return kaptchaOpen;
+    }
+
+    public void setKaptchaOpen(Boolean kaptchaOpen) {
+        this.kaptchaOpen = kaptchaOpen;
+    }
+
+
+}
